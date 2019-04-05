7a947315b66de0250f950b4fbaa9fa395f69fd35
Mehdi Khrichfa <khrichfa97@hotmail.fr>
2019-04-05 16:43:32 +0200

    Add script to map a json to an rdf graph. Currently maps publications with: creator, contributors and abstract

---
+++ process/map.py
@@ -0,0 +1,47 @@
+import json
+import pprint
+from rdflib import URIRef, Literal, Namespace, Graph
+
+# read json file
+with open('publications-sample.json', 'r') as file:
+	content = file.read()
+
+# create records list, every element in records is a dictionary
+data = json.loads(content)
+records = data['records']
+
+# define namespaces 
+GERANIUM_PUB = Namespace("http://geranium-project.org/publications/")
+GERANIUM_AUT = Namespace("http://geranium-project.org/authors/")
+PURL = Namespace("http://purl.org/dc/terms/")
+
+# create RDF graph
+graph = Graph()
+
+# list for publications URIs
+for record in records:
+	try:
+		# add publication abstract relationship
+		graph.add( (GERANIUM_PUB[str(record['handle'])], 
+		PURL.abstract, 
+		Literal(record['metadata']['dc.description.abstract'][0]['value'])) )
+		
+	except:
+		pass
+
+	# add publication creator relationship
+	author = record['internalAuthors'][0]
+	graph.add( ( GERANIUM_PUB[str(record['handle'])], 
+				PURL.creator,
+				GERANIUM_AUT[author['authority']] ) )
+
+	# add publication contributor relationship
+	for author in record['internalAuthors'][1:]:
+			graph.add( ( GERANIUM_PUB[str(record['handle'])], 
+				PURL.contributor,
+				GERANIUM_AUT[author['authority']]) )
+
+# serialize graph
+serialized = graph.serialize(format='xml')
+with open('serialized.rdf','wb') as file:
+	file.write(serialized)

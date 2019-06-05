a39d17a32d3899d695ad9dd72d6d074a9d9fd23f
Mehdi Khrichfa <khrichfa97@hotmail.fr>
2019-06-05 18:49:10 +0200

    Return JSON with improved readability from API

--- process/app.py
+++ process/app.py
@@ -1,5 +1,8 @@
 import flask
 import requests
+import json
+import sys
+from pprint import pformat
 from requests.auth import HTTPBasicAuth
 from flask import Flask
 from urllib.parse import unquote, quote
@@ -14,8 +17,25 @@ def api():
 	if flask.request.method == 'GET':
 		query = flask.request.args.get('query')
 	if query:
-		result = requests.get('https://blazegraph.nexacenter.org/blazegraph/sparql?query=' + query, auth=HTTPBasicAuth(config.db_user,config.db_password), verify=False)		
-		return result.json()
+		result = requests.get('https://blazegraph.nexacenter.org/blazegraph/sparql?format=json&query=' + query, auth=HTTPBasicAuth(config.db_user,config.db_password), verify=False)
+		data = result.json()['results']['bindings']
+		final = {}
+		for row in data:
+			if not row['id']['value'] in final:
+				final[row['id']['value']]=dict()
+				final[row['id']['value']]['id'] = row['id']['value']
+				final[row['id']['value']]['title'] = row['title']['value']
+				final[row['id']['value']]['author'] = [row['author']['value']] # list
+				final[row['id']['value']]['abstract'] = row['abstract']['value']
+				final[row['id']['value']]['topic'] = list()
+			if row['topic']['value'] not in final[row['id']['value']]['topic']:
+				final[row['id']['value']]['topic'].append(row['topic']['value'])
+			if row['coauthor']['value'] not in final[row['id']['value']]['author']:
+				final[row['id']['value']]['author'].append(row['coauthor']['value'])
+		final = json.dumps(list(final.values()))
+		with open("test.json",'w') as test:
+			test.write(final)	
+		return pformat(final)
 	return 'Invalid arguments!'
 
 if __name__ == '__main__':

b92aed00ffc0d2b0003757d178d0c8a11ce50ed7
Giuseppe Futia <giuseppe.futia@gmail.com>
2019-07-09 19:31:27 +0200

    Set number of papers and prepare code for topics

--- website/src/app/search/services/authors.service.ts
+++ website/src/app/search/services/authors.service.ts
@@ -1,18 +1,33 @@
 import { Injectable } from '@angular/core';
-import { Author } from '../models/author.model';
 import { HttpClient } from '@angular/common/http';
 import { map } from 'rxjs/operators';
 import { Observable } from 'rxjs';
 
+// Import models
+import { Author } from '../models/author.model';
+import { Topic } from '../models/topic.model';
+
+// Set interfaces to parse data
+interface Publication {
+    id: string;
+    title: string;
+    author: Author;
+    co_authors: Author[];
+    topics: Topic[];
+    submittedDate: string;
+}
+
 export interface ResponseAuthors {
   id: string;
   name: string;
-  uri: string;
+  url: string;
+  publications_on_topic: Publication[]
 }
 
 @Injectable({
   providedIn: 'root'
 })
+
 export class AuthorsService {
 
   constructor(private http: HttpClient) {}
@@ -25,7 +40,7 @@ export class AuthorsService {
    */
   getAuthorsBlock(query: string, block: number): Observable<Author[]> {
 
-    const linesPerQuery = 10;
+    const linesPerQuery = 100000;
     const linesOffset = linesPerQuery * block;
     const url = 'http://api.geranium.nexacenter.org/api?'
               + encodeURI(`type=authors&topic=${query}&lines=${linesPerQuery}&offset=${linesOffset}`);
@@ -36,19 +51,27 @@ export class AuthorsService {
       .get<ResponseAuthors[]>(url)
       .pipe(
         map(response => {
-
           const newAuthors: Author[] = [];
-
-          for(const author of response) {
+          for (const author of response) {
+            // Get the most common topics
+            var allTopics: {url: string,
+                            label: string,
+                            occ: number}[] = [];
+            for (const publication of author['publications_on_topic']) {
+              for (const topic of publication['topics']) {
+                if (allTopics.find(x=>x.url == topic.url) != undefined) {
+                  // TODO
+                }
+              }
+            }
             newAuthors.push(new Author(
               author.id,
               author.name,
-              "fake department",
+              "",
               ["fake topic"],
               "https://avatars3.githubusercontent.com/u/12415265?s=40&v=4",
-              4));
+              author['publications_on_topic'].length));
           }
-
           return newAuthors;
         })
       );
